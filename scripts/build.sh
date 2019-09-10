d7da89ebabecd53a28b76a389818a788e644abe8
Michael Wood <mawood20@gmail.com>
2019-09-10 17:46:44 -0400

    Stop build immediately if errors encountered

--- scripts/build.sh
+++ scripts/build.sh
@@ -23,31 +23,34 @@ MINI_C=${SCRIPT_DIR}/mini.c
 case $OSTYPE in
   *android*)
 
+    STDERR=/proc/self/fd/2
     MINI_EXE=${SCRIPT_DIR}/mini
 
-    make_clean
-    generate_mini_c
+    make_clean || echo "WARNING: File backups failed." > "$STDERR"
+    generate_mini_c || exit 2
 
     # Generate EXE from C source file
-    arm-linux-androideabi-clang -I /data/data/com.termux/files/usr/include/python3.7m -L /data/data/com.termux/files/usr/lib -o "${MINI_EXE}" "${MINI_C}" -lpython3.7m
+    arm-linux-androideabi-clang -I /data/data/com.termux/files/usr/include/python3.7m -L /data/data/com.termux/files/usr/lib -o "${MINI_EXE}" "${MINI_C}" -lpython3.7m || exit 3
 
-    strip "${MINI_EXE}"
+    strip "${MINI_EXE}" || exit 4
     ;;
 
   *linux*)
 
+    STDERR=/dev/stderr
     MINI_EXE=${SCRIPT_DIR}/mini
 
-    make_clean
-    generate_mini_c
+    make_clean || echo "WARNING: File backups failed." > "$STDERR"
+    generate_mini_c || exit 2
 
-    gcc -I /usr/include/python3.4m -L /usr/lib64 -o "${MINI_EXE}" "${MINI_C}" -lpython3.4m
+    gcc -I /usr/include/python3.4m -L /usr/lib64 -o "${MINI_EXE}" "${MINI_C}" -lpython3.4m || exit 3
 
-    strip "${MINI_EXE}"
+    strip "${MINI_EXE}" || exit 4
     ;;
 
   *win*)
-    echo "Windows not implemented."
+    STDERR=/dev/stderr
     MINI_EXE=${SCRIPT_DIR}/mini.exe
+    echo "Windows not implemented." > "$STDERR"
     ;;
 esac
