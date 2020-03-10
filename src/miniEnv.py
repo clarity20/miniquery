f30681a0d2f7f2dcbffe0ef4c9f26916b28cc4ec
Michael Wood <mawood20@gmail.com>
2020-03-09 21:25:45 -0400

    Adjust environment access for better Windows compatibility

--- src/miniEnv.py
+++ src/miniEnv.py
@@ -1,5 +1,5 @@
 import os
-from sys import exit
+import sys
 
 from errorManager import miniErrorManager, ReturnCode; em = miniErrorManager
 
@@ -18,36 +18,22 @@ def setEnv():
     global HOME, MINI_HOME, MINI_CACHE, MINI_CONFIG, MINI_OPTIONS
 
     # Load the optional-with-defaults settings
-    HOME = os.environ['HOME']
-    try:
-        MINI_HOME = os.environ['MINI_HOME']
-    except KeyError:
-        MINI_HOME = os.path.join(HOME, 'miniquery')
-    try:
-        MINI_CACHE = os.environ['MINI_CACHE']
-    except KeyError:
-        MINI_CACHE = os.path.join(MINI_HOME, 'cache')
-    try:
-        MINI_CONFIG = os.environ['MINI_CONFIG']
-    except KeyError:
-        MINI_CONFIG = os.path.join(MINI_HOME, 'config')
-
+    homeVariable = 'USERPROFILE' if sys.platform == 'win32' else 'HOME'
+    HOME = os.getenv(homeVariable)
+    MINI_HOME = os.getenv('MINI_HOME', HOME + os.sep + 'miniquery')
+    MINI_CACHE = os.getenv('MINI_CACHE', MINI_HOME + os.sep + 'cache')
+    MINI_CONFIG = os.getenv('MINI_CONFIG', MINI_HOME + os.sep + 'config')
     MINI_OPTIONS = _readOptionalVariable('MINI_OPTIONS')
 
     try:
-        os.environ['EDITOR']   # Make sure it is set, but don't need the value
+        os.getenv('EDITOR')   # Make sure it is set, but don't need the value
     except KeyError:
         em.doWarn('NOTE: Please set the EDITOR environment variable to your editor of choice for best results.')
 
     return ReturnCode.SUCCESS
 
 def _readOptionalVariable(varName):
-    # The try-except block is required by the language.
-    # Use 'None' to enforce the variable's optionality.
-    try:
-        return os.environ[varName]
-    except KeyError:
-        return ''
+    return os.getenv(varName, '')
 
 def setDatabaseName(name):
     global MINI_DBNAME
