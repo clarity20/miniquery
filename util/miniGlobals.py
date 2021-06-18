0c598bb2629c3865f441dc75d63b0a4448e6d0fb
Michael Wood <mawood20@gmail.com>
2021-06-18 13:06:16 -0400

    Add stub list of tqlCommands

--- util/miniGlobals.py
+++ util/miniGlobals.py
@@ -5,9 +5,25 @@
 # pertaining to MINIQUERY system commands is stored here. This is because
 # the callbacks have to be defined before we can reference them.
 #
+# MINIQUERY commands can be either explicit or implicit. Explicit commands are
+# those beginning with the "leader" prefix defined in the program settings. Any
+# input line that does NOT begin with the leader is taken to be an implicit
+# command.
+#
+# The implicit commands are a different form of TQL command that allow the
+# user to omit <leader>-commandName in front. (System commands have explicit
+# forms only.) For instance, the implicit "myTable +favoriteColumn" is
+# equivalent to the explicit "\select myTable +favoriteColumn".
+#
+# Explicit commands, on the other hand, are the sole means of invoking
+# MINIQUERY system commands.
+#
 # Format of the command list: [command name, argument syntax, short description]
 # All 3 items are used together in the "help" command.
 # The first item is used for command (auto-)completion.
+tqlCommands = [
+    'select', '', ''
+    ]
 commandList = [
     ['sq',      '<query>',        'Execute a literal SQL statement'],
     ['quit',    '',               'Exit MINIQUERY'],
