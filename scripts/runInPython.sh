d8ffe8fcc65cdf302ddec803693999f12383adb2
Michael Wood <mawood20@gmail.com>
2021-06-18 08:57:18 -0400

    Invert the sense of runInPython.sh: create temp mini file for build

--- scripts/runInPython.sh
+++ scripts/runInPython.sh
@@ -1,49 +1,52 @@
 #!/usr/bin/env bash
 
-# The main source file (mini.py) is written to be immediately buildable into
-# an executable binary using the project's build scripts.
-# This script on the other hand will run Miniquery as a python script by making a
-# temporary copy of the main source file in which we edit the import statements
-# so the interpreter can find the modules the program needs to load. This way, we don't
-# have to do a full rebuild of the project every time we want to test something.
-
-if [[ "$1" == -k ]]; then
-    keepTempFile=YES
-    shift
-fi
+# For greatest ease of development and debugging, the main source file (mini.py)
+# is designed to be a runnable python script as-is.
+# But when building the executable binary, a few changes need to be made so
+# the build process can find all the modules and transpile, compile, and link them.
+# This script makes those changes.
 
 WORKING_DIR=$HOME/projects/miniquery/scripts
 SOURCE_FILE=$WORKING_DIR/mini.py
-TEMP_SOURCE_FILE=$WORKING_DIR/temp_mini.py
+TEMP_SOURCE_FILE=$WORKING_DIR/mini_edited_for_build.py
 
-# Locate a python, giving preference to py3 over py
+# Locate python, giving preference to py3 over py
 PYTHON=$( { which python3 || which python; } 2>/dev/null) || { echo "ERROR: Python not found."; exit 2; }
 
-sed -r -e 's/includes(.*giveMini)/miniHelp\1/
-s/includes(.*Sett)/appSettings\1/
-s/includes(.*Error)/errorManager\1/
-s/includes(.*Config)/configManager\1/
-s/includes(.*argument)/argumentClassifier\1/
-s/includes(.*query)/queryProcessor\1/
-s/includes(.*miniDb)/databaseConnection\1/
-s/includes(.*stringTo)/prompts\1/
-s/utilIncludes(.*MiniComp)/miniCompleter\1/
-s/utilIncludes(.*CommandComp)/commandCompleter\1/
-s/utilIncludes(.*settingOp)/miniGlobals\1/
-s/utilIncludes(.*commandList)/miniGlobals\1/
-s/utilIncludes(.*yes_no)/miniDialogs\1/'  "${SOURCE_FILE}"  >  "${TEMP_SOURCE_FILE}"
+fileEditCommand=""
 
-if grep -q [Ii]ncludes "${TEMP_SOURCE_FILE}"; then
-    echo "************************************************************************************************"
-    echo Please add lines to $BASH_SOURCE that will convert these \"includes\" imports to the original files.
-    echo "************************************************************************************************"
-    exit 1
-fi
+function append_to_edit_command()
+{
+    importLocation="$1"
+    shift
+    importList=("$@")
+    for import in "${importList[@]}"; do
+        fileEditCommand+="s/$import /$importLocation /;"$'\n'
+    done
+}
 
-chmod a+x "${TEMP_SOURCE_FILE}"
+includeImports=(
+    miniHelp
+    appSettings
+    errorManager
+    configManager
+    argumentClassifier
+    queryProcessor
+    databaseConnection
+    prompts
+)
+utilIncludeImports=(
+    miniCompleter
+    commandCompleter
+    miniGlobals
+    miniGlobals
+    miniDialogs
+)
 
-"$PYTHON"  "${TEMP_SOURCE_FILE}"  "$@"
+append_to_edit_command  "includes"      "${includeImports[@]}"
+append_to_edit_command  "utilIncludes"  "${utilIncludeImports[@]}"
 
-if [[ "$keepTempFile" != "YES" ]]; then
-    rm -f "${TEMP_SOURCE_FILE}"
-fi
+sed -r -e "$fileEditCommand"  "${SOURCE_FILE}"  >  "${TEMP_SOURCE_FILE}"
+
+exit 0
+
