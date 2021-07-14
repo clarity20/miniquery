5892f800d5aa3c116a3b06c1ad67fe3e4e40a048
Michael Wood <mawood20@gmail.com>
2021-07-14 08:26:53 -0400

    Enhancements to errorManager: error object and more
    - Option to acceot error/exception objects to better retain error
    information
    - Factor out resetError() for more precise control by clients
    - Use underscore naming convention for private members

--- src/errorManager.py
+++ src/errorManager.py
@@ -19,7 +19,7 @@ class ReturnCode(Enum):
     USER_EXIT = 10
     DATABASE_CONNECTION_ERROR = 11
     MISSING_SETTINGS_FILE = 12
-    DB_DRIVER_RAISED_ERROR = 13
+    DB_DRIVER_ERROR = 13
     PROMPT_ERROR = 14
     MISSING_PASSWORD = 15
     ILLEGAL_ARGUMENT = 16
@@ -42,7 +42,7 @@ errorMsgDict = {
     10 : '\nThank you for using MINIQUERY!\n',
     11 : 'Database connection error: {0}, {1}',
     12 : 'Application settings file "{0}" missing or unreadable.',
-    13 : 'Error/exception thrown by {0} driver:\n  error type:   {1}\n  full type / desc: {2}\n  command:   {3}',
+    13 : 'Error/exception thrown by {0} db driver:',
     14 : 'Illegal {0} "{1}" in prompt.',
     15 : 'Unable to prompt for password. Please specify it using "-p" option or config file.',
     16 : 'Illegal argument. {0} is required.',  # This should be specialized to the context
@@ -55,56 +55,86 @@ errorMsgDict = {
 class ErrorManager:
 
     def __init__(self):
-        self.errMsg = ''
-        self.returnCode = ReturnCode.SUCCESS
-        self.errOutputStream = sys.stderr
+        self._errMsg = ''
+        self._exception = None
+        self._returnCode = ReturnCode.SUCCESS
+        self._errOutputStream = sys.stderr
+
+    def setException(self, exc, msg):
+        '''
+        A pretty flexible approach to exception handling that can make full use of the thrown exception object
+        '''
+        SQLALCHEMY_ERROR_TEMPLATE = '  error type:   {0}\n  full descr: {1}\n  SQL command:   {2}'
+
+        from sqlalchemy.exc import DBAPIError
+        self._exception = exc
+        exceptionType = type(exc).__name__
+
+        # Set the err msg based on the exception type
+        if isinstance(exc, DBAPIError):
+            # Handle errors raised by the DBAPI (and passed on by SQLAlchemy).
+            # See the SQLAlchemy help pages and website for further information.
+            self._errMsg = msg + ":\n" + SQLALCHEMY_ERROR_TEMPLATE.format(exceptionType, exc.args[0], exc.statement)
+            self._returnCode = ReturnCode.DB_DRIVER_ERROR
+        else:
+            print('Handling for exception type %s not implemented yet.' % exceptionType)
+
+        return self._returnCode
+
+    def getException(self):
+        return self._exception
 
     def setError(self, code, *args, msgOverride=""):
+        '''
+        A vestigial approach to exception handling. Captures less information than objects.
+        '''
         if '$' in msgOverride:
             print('Bad call to setError: argument "msg" must be flat.')
             return 99
-        else:
-            pass
 
         if msgOverride != '':
-            self.errMsg = msgOverride
-            self.returnCode = code
+            self._errMsg = msgOverride
+            self._returnCode = code
         else:
             template = errorMsgDict[code.value]
-            self.errMsg = template.format(*args) if args else template
-            self.returnCode = code
+            self._errMsg = template.format(*args) if args else template
+            self._returnCode = code
 
-        return self.returnCode
+        return self._returnCode
 
     def getError(self):
-        return self.returnCode
+        return self._returnCode
 
     def doExit(self, msg=None):
-        if self.errOutputStream.isatty() and self.returnCode.value:
-            if self.returnCode == ReturnCode.USER_EXIT:
+        if self._errOutputStream.isatty() and self._returnCode.value:
+            if self._returnCode == ReturnCode.USER_EXIT:
                 color = 'green' if platform.system() == 'Windows' else 'lightgreen'
             else:
                 color = 'red'
-            print_formatted_text(FormattedText([(color, msg or self.errMsg)]),
-                                file=self.errOutputStream)
+            print_formatted_text(FormattedText([(color, msg or self._errMsg)]),
+                                file=self._errOutputStream)
         else:
-            print(msg if msg else self.errMsg, file=self.errOutputStream)
+            print(msg if msg else self._errMsg, file=self._errOutputStream)
 
-        sys.exit(0 if self.returnCode == ReturnCode.USER_EXIT
-                     else self.returnCode.value)
+        sys.exit(0 if self._returnCode == ReturnCode.USER_EXIT
+                     else self._returnCode.value)
 
-    # Warn: For situations that warrant giving the user a chance to
-    # fix things up instead of totally exiting the program
+    # Warn: For error conditions or warnings that should be nonfatal
     def doWarn(self, msg=None):
-        if self.errOutputStream.isatty() and self.returnCode.value:
-            color = 'yellow' if self.returnCode == ReturnCode.Clarification else 'red'
-            print_formatted_text(FormattedText([(color, msg or self.errMsg)]),
-                                file=self.errOutputStream)
+        if self._errOutputStream.isatty() and self._returnCode.value:
+            color = 'yellow' if self._returnCode == ReturnCode.Clarification else 'red'
+            print_formatted_text(FormattedText([(color, msg or self._errMsg)]),
+                                file=self._errOutputStream)
         else:
-            print(msg if msg else self.errMsg, file=self.errOutputStream)
+            print(msg if msg else self._errMsg, file=self._errOutputStream)
 
+        self.resetError()
+
+    def resetError(self):
         # Wipe out the error state so the user can continue
-        self.returnCode = ReturnCode.SUCCESS
-        self.errMsg = ''
+        self._returnCode = ReturnCode.SUCCESS
+        self._exception = None
+        self._errMsg = ''
+
 
 miniErrorManager = ErrorManager()
